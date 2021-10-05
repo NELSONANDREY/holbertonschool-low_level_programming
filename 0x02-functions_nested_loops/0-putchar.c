498eee92a4baf9609c428b1418ffc01195f68c54
NELSONANDREY <3424@holbertonschool.com>
2021-10-05 12:14:18 -0500

    first folders

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
-/**documentation**/
+/**
+ * main: the function principal in the program holbeton school.
+ * void: documentation.
+ * return: 0.
+ **/
+
 int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-    return(0);
+return (0);
 }
