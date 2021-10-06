3ec4cbdd968a7a6d23d0879ace0e5d74d0641138
NELSONANDREY <3424@holbertonschool.com>
2021-10-05 20:19:02 -0500

    Write a function that prints the alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * print_alphabet - check the code.
  * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
-    int i = 0;
-    for(i= 'a'; i<= 'z'; i++)
-    {
-        _putchar (i);
-    }
-    _putchar ('\n');
+int i = 0;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar (i);
+}
+_putchar ('\n');
 }
