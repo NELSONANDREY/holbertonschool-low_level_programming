d3afff509de6f012206ebbb443c2128f1bfb9994
NELSONANDREY <3424@holbertonschool.com>
2021-10-14 22:06:59 -0500

    folders every day23s

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,14 +6,16 @@
  * @s2: input two
  * Return: Always 0 (Success)
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (s1[i] != 0 && s2[i] != 0)/* B */
 	{
-		if (s1[i] != s2[i])
+		if (s1[i] != s2[i])/* C */
 			return (s1[i] - s2[i]);
+		i++;
 	}
 	return (0);
 }
