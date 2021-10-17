bf3457840f2a238cc106d1ceeb4361dcccd7fb19
NELSONANDREY <3424@holbertonschool.com>
2021-10-16 21:38:36 -0500

    function that encodes a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - Entry point
+ * ONE if, TWO loops only...
+ * @n: input
+ * Return: Always 0 (Success)
+ */
+char *leet(char *n)
+{
+    char lts[] = "aAeEoOtTlL";
+    char nro[] = "4433807711";
+    int i, j;
+
+    for(i = 0; n[i] != '\0'; i++)
+    {
+        for(j = 0; j < 10; j++)
+        {
+            if (n[i] == lts[j])
+                n[i] = nro[j];
+        }
+    }
+    return (n);
+}
