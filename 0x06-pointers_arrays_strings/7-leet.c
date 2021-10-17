e0ddc7056223fad23baec974843c19c317423283
NELSONANDREY <3424@holbertonschool.com>
2021-10-16 21:49:59 -0500

    function that encodes a string into 133_7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,17 +8,17 @@
  */
 char *leet(char *n)
 {
-    char lts[] = "aAeEoOtTlL";
-    char nro[] = "4433807711";
-    int i, j;
+	char lts[] = "aAeEoOtTlL";
+	char nro[] = "4433807711";
+	int i, j;
 
-    for(i = 0; n[i] != '\0'; i++)
-    {
-        for(j = 0; j < 10; j++)
-        {
-            if (n[i] == lts[j])
-                n[i] = nro[j];
-        }
-    }
-    return (n);
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (n[i] == lts[j])
+				n[i] = nro[j];
+		}
+	}
+	return (n);
 }
