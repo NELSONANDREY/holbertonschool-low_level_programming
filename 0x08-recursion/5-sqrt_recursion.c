8718c8eb6dfec3f197862d89a5a892c448fe15d0
NELSONANDREY <3424@holbertonschool.com>
2021-10-21 22:03:47 -0500

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,5 @@
 #include "main.h"
 /**
- * _sqrt_recursion - no loop, 1. 
  * aux - checks for perfect square
  * @n: input
  * Return: Always 0 (Success)
@@ -10,10 +9,16 @@ int aux(int n, int i)
 	if (i * i == n)
 	return (i);
 	else if (i < n)
-	aux(n, i + 1);
+	return (aux(n, i + 1));
 	else
 	return (-1);
 }
+/**
+ * _sqrt_recursion - no loop, 1. 
+ * @i: input
+ * @n: input
+ * Return: Always 0 (Success)
+ */
 int _sqrt_recursion(int n)
 {
 	int i;
