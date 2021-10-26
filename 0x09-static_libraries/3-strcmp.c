8863d6075f005f3d63a842c7c55706740ea06dfb
NELSONANDREY <3424@holbertonschool.com>
2021-10-26 17:50:15 -0500

    every folders

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcmp - a function that compares two strings
+ * @s1: input one
+ * @s2: input two
+ * Return: Always 0 (Success)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	while (s1[i] != 0 && s2[i] != 0)/* B */
+	{
+		if (s1[i] != s2[i])/* C */
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
