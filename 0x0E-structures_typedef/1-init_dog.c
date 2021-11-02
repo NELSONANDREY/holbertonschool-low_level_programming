7ee64aff9cd7046573c431a41a5ff45af1730597
NELSONANDREY <3424@holbertonschool.com>
2021-11-01 19:20:18 -0500

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif /* DOG_H */
