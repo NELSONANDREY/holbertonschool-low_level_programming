61c889289a97fbdceec48369a5a12e76a2614bfd
NELSONANDREY <3424@holbertonschool.com>
2021-11-19 19:51:22 -0500

    main2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,29 @@
 #include "main.h"
+
 /**
- * binary_to_uint - Entry Point
- * @b: const char
- * Return: 0
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b:pointing to a string of 0 and 1 chars.
+ * Return: the converted number, or 0 if.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int counter = 0;
-	int exp = 1;
-	/**para que arranque en 1 ya que si se deja en cero nunca va a aumentar*/
-	int change = 0;/*para guardar cambios a las variables*/
+	int counter = 0, exp = 1, change = 0;
 
 	if (b == NULL)
 	{
-		return (0);/* code */
+		return (0);
 	}
 	while (b[counter] != '\0')
-	/*b en la posicion de contador va a recorrer hasta que sea nulo \0*/
 	{
 		counter++;
 	}
-	counter -= 1;/* */
+	counter -= 1;
 	while (counter >= 0)
 	{
 		if (b[counter] != '0' && b[counter] != '1')
 			return (0);
 		change += ((b[counter] - 48) * exp);
-		/*multiplicar la variable que encontro en la posicion por, */
-		exp *= 2;/*para que cuente las posiciones y se vaya elevando*/
+		exp = exp * 2;
 		counter--;
 	}
 	return (change);
