50a8481b267644a13b6e90166cf963eec6614833
NELSONANDREY <3424@holbertonschool.com>
2021-11-23 14:48:50 -0500

    main and 1-create1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (i=0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i] != '\0'; i++)
 					;
 			wr = write(file, text_content, i);
 			if (wr == -1)
