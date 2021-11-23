d5c6d7d62a8d0472ff14c5b2a6d7e76304fcb9e5
NELSONANDREY <3424@holbertonschool.com>
2021-11-23 15:07:07 -0500

    main and 1-create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,22 +7,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file, wr, i;
+	int file, wr, j;
 
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (file == -1)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
-					;
-			wr = write(file, text_content, i);
-			if (wr == -1)
-				return (-1);
+		for (j = 0; text_content[j] != '\0'; j++)
+			;
+		wr = write(file, text_content, j);
+		if (wr == -1)
+			return (-1);
 	}
 
 	close(file);
