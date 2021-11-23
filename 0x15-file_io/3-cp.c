5aa6b74d42f2aed28feeae13acf83bcc0bb4e3bc
NELSONANDREY <3424@holbertonschool.com>
2021-11-23 17:57:26 -0500

    delete point 2

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * append_text_to_file - appends text to a file
+  * @filename: file to append to, if NULL, return -1
+  * @text_content: content to append, if NULL, do not append
+  * Return: 1 on success, -1 on failure
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int plus, j, wr;
+
+	if (filename == NULL)
+		return (-1);
+	plus = open(filename, O_WRONLY | O_APPEND);
+	if (plus == -1)
+		return (-1);
+	if (text_content == NULL)
+		return (1);
+	for (j = 0; text_content[j]; j++)
+		;
+	wr = write(plus, text_content, j);
+	close(plus);
+	return (wr == -1 ? -1 : 1);
+}
